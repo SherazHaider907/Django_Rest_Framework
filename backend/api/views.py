5d1f6b5ebe995097cf79ffb42ab8fee05f63580f
SherazHaider907 <Sherazhaider665@gmail.com>
2026-04-22 15:37:17 +0500

    add serializers and form files in the project

--- backend/api/views.py
+++ backend/api/views.py
@@ -14,7 +14,7 @@ def api_home(request, *args, **kwargs):
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
-        data = model_to_dict(model_data, fields=['id', 'title', 'content', 'price'])
+        data = model_to_dict(model_data, fields=['id', 'title', 'content', 'price', 'sale_price'])
     return Response(data)
 
 
--- backend/products/models.py
+++ backend/products/models.py
@@ -5,3 +5,8 @@ class Product(models.Model):
     title = models.CharField(max_length=120)
     content = models.TextField(blank=True, null=True)
     price = models.DecimalField(decimal_places=2, max_digits=20, default=99.99)
+
+
+    @property
+    def sale_price(self):
+        return "%.2f" % (float(self.price) * 0.8)
