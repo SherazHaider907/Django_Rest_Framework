4ac3e46772d2fc73bb09da49214541d3a43a78a7
SherazHaider907 <Sherazhaider665@gmail.com>
2026-04-22 11:36:32 +0500

    django

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,14 +1,15 @@
 import json
-from django.http import JsonResponse
+from django.forms.models import model_to_dict
+from django.http import JsonResponse, HttpResponse
+
 from products.models import Product
 def api_home(request, *args, **kwargs):
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
-        data['id'] = model_data.id
-        data['title'] = model_data.title
-        data['content'] = model_data.content
-        data['price'] = model_data.price
+        data = model_to_dict(model_data, fields=['id', 'title', 'content', 'price'])
+    #     json_data_str = json.dumps(data)
+    # return HttpResponse(json_data_str, header={"Content-Type": "application/json"})
     return JsonResponse(data)
 
 
@@ -72,6 +73,41 @@ def api_home(request, *args, **kwargs):
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import json
+# from django.http import JsonResponse
+# from products.models import Product
+# def api_home(request, *args, **kwargs):
+#     model_data = Product.objects.all().order_by("?").first()
+#     data = {}
+#     if model_data:
+#         data['id'] = model_data.id
+#         data['title'] = model_data.title
+#         data['content'] = model_data.content
+#         data['price'] = model_data.price
+#     return JsonResponse(data)
 
 
 # def api_home(request, *args, **kwargs):
--- py_client/basic.py
+++ py_client/basic.py
@@ -6,7 +6,7 @@ import requests
 endpoint = "http://localhost:8000/api/"
 
 response = requests.get(endpoint,params={"abc":123},json={"query":"hello world"})
-
+# print(response.headers)
 # print(response.text)
 # print(response.status_code)
 
